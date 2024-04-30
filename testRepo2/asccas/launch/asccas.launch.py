cad1cddab60646a6bf6104f35b85ce0cdde5bede
TheoMangos <130418904+TheoMangos@users.noreply.github.com>
2024-04-30 10:14:46 +1000

    committing

---
+++ testRepo2/asccas/launch/asccas.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='asccas',
+            executable='Node1.py',
+            name='node1'
+        ),
+        Node(
+            package='asccas',
+            executable='Node2.py',
+            name='node2'
+        ),
+
+        # customize launch file below
+
+        # end launch file customization
+    ])
